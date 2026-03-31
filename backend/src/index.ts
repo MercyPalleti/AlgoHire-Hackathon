b8bc0bbace5001c1f45c1c37651eb7aafb3704ed
MercyPalleti <palletimercy1@gmail.com>
2026-03-31 12:28:15 +0530

    routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", ingestRoutes);
-app.use("/api/alerts", alertRoutes); // ✅ ADD THIS
+app.use("/api/alerts", alertRoutes);
 app.use("/api/history", historyRoutes);
 
 const server = http.createServer(app);
